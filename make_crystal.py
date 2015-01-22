4648ff6c75a366e9c7bbeef0648be87bc59ff690
latashyoussef <latashyoussef@gmail.com>
2015-01-23 00:58:03 +1100

    Update make_crystal.py

--- make_crystal.py
+++ make_crystal.py
@@ -5,13 +5,13 @@ L = 3.2		# Length of the cylinder
 
 def make_row_x(box_length, L, D):
 	rod_count = 0
-	x = D
+	x = 0.5*D
 	while x < box_length:
+		x += D
 		y = L/2.0
 		z = D/2.0
 		print "%f %f %f \n" % (x, y, z)
 		rod_count += 1
-		x += 2*D
 	return rod_count
 
 
