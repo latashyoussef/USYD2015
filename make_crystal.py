0627d86e52c535070e685a009d6f6ea203240247
latashyoussef <latashyoussef@gmail.com>
2015-01-23 00:59:06 +1100

    Update make_crystal.py

--- make_crystal.py
+++ make_crystal.py
@@ -7,11 +7,11 @@ def make_row_x(box_length, L, D):
 	rod_count = 0
 	x = 0.5*D
 	while x < box_length:
-		x += D
 		y = L/2.0
 		z = D/2.0
 		print "%f %f %f \n" % (x, y, z)
 		rod_count += 1
+		x += D
 	return rod_count
 
 
